84e6e9463ce564317a1030be8a44dfe871bb8d63
gao_xianglong <gao_xianglong@sina.com>
2019-08-09 14:20:13 +0800

    修改启动脚本

--- corgi-proxy-server/src/main/assembly/bin/start.sh
+++ corgi-proxy-server/src/main/assembly/bin/start.sh
@@ -9,6 +9,36 @@ if [ -f "$NOHUP_PATH" ];then
   rm -drf $NOHUP_PATH
 fi
 
+if [ -z "$CORGI_HOME" ] ; then
+  ## resolve links - $0 may be a link to maven's home
+  PRG="$0"
+
+  # need this for relative symlinks
+
+  # need this for relative symlinks
+  while [ -h "$PRG" ] ; do
+    ls=`ls -ld "$PRG"`
+    link=`expr "$ls" : '.*-> \(.*\)$'`
+    if expr "$link" : '/.*' > /dev/null; then
+      PRG="$link"
+    else
+      PRG="`dirname "$PRG"`/$link"
+    fi
+  done
+
+  saveddir=`pwd`
+
+  CORGI_HOME=`dirname "$PRG"`/..
+
+  # make it fully qualified
+  CORGI_HOME=`cd "$CORGI_HOME" && pwd`
+
+  cd "$saveddir"
+fi
+
+export CORGI_HOME
+cd $CORGI_HOME
+
 JAVA_OPS="-Dversion=0.0.1 -Dio.netty.leakDetectionLevel=SIMPLE -Xms2g -Xmx2g -Xmn768m -Xss256k -XX:+UseParallelOldGC -XX:ParallelGCThreads=3 -XX:+HeapDumpOnOutOfMemoryError -XX:+PrintGCDetails -XX:+PrintGCDateStamps -Xloggc:logs/gc.log -Djava.ext.dirs=libs -classpath conf com.github.registry.corgi.server.Main"
 
 nohup java -server $JAVA_OPS &
